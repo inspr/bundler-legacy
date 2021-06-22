e353005e580c8fe9b734d8fe1107c476a0dfa127
Breno Jesus <breno@inspr.com>
2021-06-21 22:39:20 -0300

    feat(disk): add operator to dump files

--- pkg/disk/disk.go
+++ pkg/disk/disk.go
@@ -3,6 +3,7 @@ package disk
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"inspr.dev/primal/pkg/api"
 )
@@ -26,7 +27,22 @@ func (h *Disk) Apply(ctx context.Context, opts api.OperatorOptions) error {
 	// fmt.Println(opts.Files)
 
 	h.meta.Messages <- fmt.Sprintf("writing files to %s", h.path)
-	opts.Files.Flush(h.path)
+
+	if _, err := os.Stat(h.path); os.IsNotExist(err) {
+		os.Mkdir(h.path, 0755)
+		os.Mkdir(h.path+"/assets", 0755)
+	}
+
+	for key, file := range opts.Files.Raw() {
+		f, err := os.Create(h.path + key)
+
+		if err != nil {
+			return err
+		}
+
+		f.Write(file)
+	}
+	// opts.Files.Flush(h.path)
 
 	h.meta.State <- api.DONE
 	h.meta.Progress <- 1.0
