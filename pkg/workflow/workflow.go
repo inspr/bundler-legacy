69e36fb1e43f23ebdc2d61374e3f5d634dcda857
Francisco Andrade <francisco@inspr.com>
2021-07-29 13:51:37 -0300

    ref(workflow): moved structs and interfaces to new file

--- pkg/workflow/workflow.go
+++ pkg/workflow/workflow.go
@@ -4,40 +4,6 @@ import (
 	"fmt"
 )
 
-// Status is an enum for possible Operators run status
-type Status int
-
-const (
-	IDLE Status = 1 << iota
-	RUNNING
-	DONE
-)
-
-// WorkflowInterface defines methods to manage and run a workflow
-type WorkflowInterface interface {
-	Add(Task)
-	Remove(Task)
-	Run()
-}
-
-// Task defines an operator's execution methods and its needed data
-type Task struct {
-	ID     string
-	Before func()
-	After  func()
-
-	Run func(*Task)
-
-	DependsOn []*Task
-
-	State Status
-}
-
-// Workflow is a set of tasks with a predefined order of execution
-type Workflow struct {
-	Tasks []*Task
-}
-
 // Add adds a new task in a workflow
 func (w *Workflow) Add(task Task) {
 	w.Tasks = append(w.Tasks, &task)
