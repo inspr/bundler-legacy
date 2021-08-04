9a5fbf36863acf1771adc40b6849b1a1eade7f95
Francisco Andrade <francisco@inspr.com>
2021-08-04 11:30:59 -0300

    misc(vm): added comments to describe how vm works

--- lib/web/server/vm.go
+++ lib/web/server/vm.go
@@ -31,13 +31,15 @@ func (vm *VM) Handler(fs filesystem.FileSystem) {
 
 	runFn, _ := v8go.NewFunctionTemplate(iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
 		html := fmt.Sprintf("%v", info.Args()[0]) // when the JS function is called this Go callback will execute
+		// ! this generates the html file to be served by the server
 		fmt.Println("\n" + html + "\n")
 		return nil // you can return a value back to the JS caller if required
 	})
 
 	global, _ := v8go.NewObjectTemplate(iso) // a template that represents a JS Object
-	global.Set("run", runFn)                 // sets the "print" property of the Object to our function
-	ctx1, _ := v8go.NewContext(iso, global)  // new Context with the global Object set to our object template
+	// ! every html page must have a "run" declared in the js file
+	global.Set("run", runFn)                // sets the "print" property of the Object to our function
+	ctx1, _ := v8go.NewContext(iso, global) // new Context with the global Object set to our object template
 
 	_, err := ctx1.RunScript(string(vm.file), "stdin.js")
 	if err != nil {
