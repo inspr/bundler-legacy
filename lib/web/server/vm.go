22f8d474f1298e4101d2c239f804ed3889da458f
Francisco Andrade <francisco@inspr.com>
2021-08-05 10:46:55 -0300

    dev(vm): started new vm structure

--- lib/web/server/vm.go
+++ lib/web/server/vm.go
@@ -1,48 +1,61 @@
 package operator
 
 import (
-	"fmt"
+	"context"
+	"net/http"
 
-	"inspr.dev/primal/pkg/filesystem"
 	"rogchap.com/v8go"
 )
 
-// TODO: modify the structure so VM is initialized and can accept new contexts
-// based on files inputed in the application server
-
 // VM defines the vm structure and its fields
 type VM struct {
-	path string
-	file []byte
+	mux *http.ServeMux
+	ctx context.Context
+	iso *v8go.Isolate
 }
 
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
 // NewVM returns a reference to a new VM structure with the given path and file
-func NewVM(path string, file []byte) *VM {
+func NewVM(ctx context.Context) *VM {
+	iso, _ := v8go.NewIsolate()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/vm", VMHandler())
+
 	return &VM{
-		path,
-		file,
+		mux: mux,
+		ctx: ctx,
+		iso: iso,
 	}
 }
 
 // Handler handles js code in the given filesystem
-func (vm *VM) Handler(fs filesystem.FileSystem) {
-	iso, _ := v8go.NewIsolate() // creates a new JavaScript VM
-	defer iso.Dispose()
+func VMHandler() HandlerFunc {
 
-	runFn, _ := v8go.NewFunctionTemplate(iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
-		html := fmt.Sprintf("%v", info.Args()[0]) // when the JS function is called this Go callback will execute
-		// ! this generates the html file to be served by the server
-		fmt.Println("\n" + html + "\n")
-		return nil // you can return a value back to the JS caller if required
+	return HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 	})
+	// iso, _ := v8go.NewIsolate() // creates a new JavaScript VM
+	// defer iso.Dispose()
 
-	global, _ := v8go.NewObjectTemplate(iso) // a template that represents a JS Object
-	// ! every html page must have a "run" declared in the js file
-	global.Set("run", runFn)                // sets the "print" property of the Object to our function
-	ctx1, _ := v8go.NewContext(iso, global) // new Context with the global Object set to our object template
+	// runFn, _ := v8go.NewFunctionTemplate(iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
+	// 	html := fmt.Sprintf("%v", info.Args()[0]) // when the JS function is called this Go callback will execute
+	// 	// ! this generates the html file to be served by the server
+	// 	fmt.Println("\n" + html + "\n")
+	// 	return nil // you can return a value back to the JS caller if required
+	// })
 
-	_, err := ctx1.RunScript(string(vm.file), "stdin.js")
-	if err != nil {
-		fmt.Println(err)
-	}
+	// global, _ := v8go.NewObjectTemplate(iso) // a template that represents a JS Object
+	// // ! every html page must have a "run" declared in the js file
+	// global.Set("run", runFn)                // sets the "print" property of the Object to our function
+	// ctx1, _ := v8go.NewContext(iso, global) // new Context with the global Object set to our object template
+
+	// _, err := ctx1.RunScript(string(vm.file), "stdin.js")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+}
+
+func (vm *VM) Run() {
+
 }
